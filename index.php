6adb589f2ea38d22395494d6e9baad75ab7d7497
luisdiego99 <pytests99@gmail.com>
2024-03-12 00:35:30 -0600

    Add code and image

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "Hello World!";
+
+    $imagePath = "/home/pytests99/Downloads/skeletonjazz.jpg"; 
+    echo '<br><img src="' . $imagePath . '" alt="Skeletons playing jazz">';
+?>
+</body>
+</html>
